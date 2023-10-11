5c8545aee7631dc9b9d2ef2dc06b1cffc7ae0630
GugaSantos <guga.santos.guga@gmail.com>
2023-10-10 23:11:36 -0300

    feat: recording json at usestate

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -1,29 +1,47 @@
-'use client'
+'use client';
+
+import { useState } from 'react';
 
 export const UploadButton = () => {
+  const [file, setFile] = useState<any>(null);
 
-  function handleFileUpload() {
-    
+  function handleFileUpload(event: React.ChangeEvent<HTMLInputElement>) {
+    const selectedFile = event.target.files?.[0];
+
+    if (selectedFile) {
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+
+        if (e.target) {
+          const jsonContent = e.target.result as string;
+
+          try {
+            const jsonObject = JSON.parse(jsonContent);
+            setFile(jsonObject);
+          } catch (error) {
+            console.error('Erro ao analisar o JSON:', error);
+          }
+        }
+      };
+      reader.readAsText(selectedFile);
+    }
   }
 
   return (
     <div
-      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded hover:cursor-pointer"
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer"
     >
       <input
         type="file"
         accept=".json"
-        style={{ display: 'none' }}
         id="jsonUpload"
-        onChange={() => handleFileUpload()}
-        className="select-none"
-        />
-      <label 
-      htmlFor="jsonUpload"
-      className="select-none"
-      >
+        onChange={(e) => handleFileUpload(e)}
+        className="select-none cursor-pointer hidden"
+      />
+      <label htmlFor="jsonUpload" className="select-none">
         Load JSON
       </label>
     </div>
-  )
-}
+  );
+};
